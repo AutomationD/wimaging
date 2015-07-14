388f35153f23508b2993ab4fcd66cb78be3a4ecc
Michal Nowak <mikenowak@users.noreply.github.com>
2015-07-14 23:36:52 +0000

    Adding support for custom OS drive letter

--- tools/install.root/Windows/Setup/Scripts/setupcomplete.cmd
+++ tools/install.root/Windows/Setup/Scripts/setupcomplete.cmd
@@ -1,3 +1,7 @@
 @echo off
-set wimagingRoot=C:\wimaging
+
+:: Find the OS drive letter
+for /f "tokens=1 delims=\" %%D in ("%WINDIR%") do SET OSDrive=%%D
+
+set wimagingRoot=%OSDrive%\wimaging
 call %wimagingRoot%\deploy\10_init.cmd
