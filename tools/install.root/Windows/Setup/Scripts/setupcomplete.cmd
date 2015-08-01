3ae00005cc1b9862447c96329cbb8ed9af618f8b
Michal Nowak <mikenowak@users.noreply.github.com>
2015-08-01 09:38:13 +0000

    Simplified path setup

--- tools/install.root/Windows/Setup/Scripts/setupcomplete.cmd
+++ tools/install.root/Windows/Setup/Scripts/setupcomplete.cmd
@@ -1,7 +1,3 @@
 @echo off
 
-:: Find the OS drive letter
-for /f "tokens=1 delims=\" %%D in ("%WINDIR%") do SET OSDrive=%%D
-
-set wimagingRoot=%OSDrive%\wimaging
-call %wimagingRoot%\deploy\10_init.cmd
+call c:\wimaging\deploy\10_init.cmd
