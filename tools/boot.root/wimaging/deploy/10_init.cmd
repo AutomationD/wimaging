8162555b22d460189f86eb0cbd4842c35823d973
Daniel Helgenberger <daniel.helgenberger@m-box.de>
2015-09-25 18:34:12 +0200

    Remove all %DeploymentShare% references -- DANIEL HELGENBERGER <daniel.helgenberger@m-box.de>

--- tools/boot.root/wimaging/deploy/10_init.cmd
+++ tools/boot.root/wimaging/deploy/10_init.cmd
@@ -8,16 +8,16 @@ wpeinit
 echo Starting networking...
 :testagain
 ipconfig /renew
-ping -n 1 %deploymentShare% > NUL
+ping -n 1 %foremanHost% > NUL
 if %errorlevel% == 0 goto pingok
 REM wait 3 sec. and try it again
 timeout /NOBREAK /t 3
 goto testagain
 :pingok
-for /f "delims=[] tokens=2" %%a in ('ping -4 %deploymentShare% -n 1 ^| findstr "["') do (set thisip=%%a)
-echo Ping to %deploymentShare% OK! Your IP address: %thisip%
+for /f "delims=[] tokens=2" %%a in ('ping -4 %foremanHost% -n 1 ^| findstr "["') do (set thisip=%%a)
+echo Ping to %foremanHost% OK! Your IP address: %thisip%
 
 
 echo Downloading Setup Script
-%deployRoot%\wget64.exe http://%foremanHost%/unattended/script -O %deployRoot%\peSetup.cmd
+%deployRoot%\wget64.exe --no-verbose http://%foremanHost%/unattended/script -O %deployRoot%\peSetup.cmd
 call %deployRoot%\peSetup.cmd
